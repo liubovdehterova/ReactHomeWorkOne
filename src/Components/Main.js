2d629167c69f04a182c98673930a3af1526f8856
liubovdehterova <lubovdegtereva60@gmail.com>
2022-02-23 17:40:31 +0200

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <main className="main">
+            <div className="conteiner">
+                <div className="main__titel">
+                    Title
+                </div>
+                <div className="main__info">
+                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid beatae, consequatur cumque
+                        debitis, delectus dolorum error et in ipsam iure modi perferendis quia quo rem similique
+                        suscipit tempora voluptatum? Minus.
+                    </div>
+                    <div>Accusamus asperiores dolor eveniet illo inventore ipsam nisi nulla, perspiciatis ratione
+                        repellat sit soluta, unde vitae? Accusamus eos esse et, omnis porro quam quas ratione sit. Dicta
+                        maxime perspiciatis quisquam!
+                    </div>
+                    <div>Ab accusantium adipisci amet architecto autem consequuntur cumque, delectus dolores ea error ex
+                        exercitationem explicabo, iste itaque iusto nisi rem repellat saepe sint temporibus tenetur
+                        velit voluptatum. Molestiae, perferendis, quibusdam.
+                    </div>
+                    <div>Aliquid animi atque aut beatae blanditiis commodi debitis deserunt dolore ducimus eos est
+                        eveniet, excepturi harum laborum maiores nam porro quae recusandae repellendus sapiente,
+                        suscipit tempora tenetur veritatis voluptas voluptate.
+                    </div>
+                    <div>Ab accusamus aliquid animi architecto, asperiores, consequuntur culpa dolor, eius eligendi esse
+                        fuga hic impedit in ipsum laborum minus natus nihil nisi non nostrum quidem quo rerum sint ut
+                        veritatis.
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Main;
